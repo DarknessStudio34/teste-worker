c107570bcc5b4e7e00f041403a00d8eaa3c7cd7a
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-18 14:16:33 -0300

    Update teste.js

--- codes/teste.js
+++ codes/teste.js
@@ -3,12 +3,7 @@ var graphics = {};
 
 function setup() {
   createCanvas(800, 360);
-  var worker = new Worker('codes/load-image-worker.js');
-  worker.postMessage('assets/teste.png');
-  worker.onmessage = function(event) {
-    graphics.bg = event.data;
-    c = 1;
-  };
+  mouseClicked(loadImage);
 }
 
 function draw() {
@@ -16,4 +11,13 @@ function draw() {
   if (c == 1) {
     background(graphics.bg);
   }
+}
+
+function loadImage() {
+  var worker = new Worker('codes/load-image-worker.js');
+  worker.postMessage('assets/teste.png');
+  worker.onmessage = function(event) {
+    graphics.bg = event.data;
+    c = 1;
+  };
 }
