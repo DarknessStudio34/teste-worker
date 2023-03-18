7e838864ee8bfade46f16662cd69b0e870840d4f
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-03-18 12:55:32 -0300

    Update teste.js

--- codes/teste.js
+++ codes/teste.js
@@ -1,6 +1,19 @@
-function setup () {
+var c = 0;
+var graphics = {};
+
+function setup() {
   createCanvas(800, 360);
+  var worker = new Worker('codes/load-image-worker.js');
+  worker.postMessage('assets/teste.png');
+  worker.onmessage = function(event) {
+    graphics.bg = event.data;
+    c = 1;
+  };
 }
-function draw (){
-  
+
+function draw() {
+  background(220);
+  if (c == 1) {
+    background(graphics.bg);
+  }
 }
